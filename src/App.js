a9c6ca2c98c3623484b10be8cc9aff1023cd801d
Tyler Randall <tyler7r@gmail.com>
2023-04-11 10:59:42 -0400

    Set up firebase hosting and create google sign in

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithPopup, signOut } from 'firebase/auth';
+import { db } from './firebase';
+
 function App() {
+  const signIn = async () => {
+    const provider = new GoogleAuthProvider();
+    await signInWithPopup(getAuth(), provider);
+  }
+
   return (
     <div className="App">
-      
+      <div onClick={() => signIn()} id='sign-in-button'>Sign In</div>
     </div>
   );
 }
